561a1e7b130447da217219b016f7173cfeba5697
Benedict Owusu <beneyjoy@gmail.com>
2024-04-22 22:44:12 +0000

    task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,9 @@
-function getNeighborhoodsList() {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
   const self = this;
-  this.addNeighborhood = add = (newNeighborhood) =>
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
-
+  };
 }
-
-console.log(getNeighborhoodsList());
