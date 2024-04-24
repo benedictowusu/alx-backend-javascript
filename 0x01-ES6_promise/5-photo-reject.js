2f67aadfd0dd4edee5b5f253d364b9d60ab8bf3a
Benedict Owusu <beneyjoy@gmail.com>
2024-04-24 16:07:15 +0000

    task 4

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(Error(`${filename} cannot be processed`));
+export default function uploadPhoto(fileName) {
+  return new Promise((reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
