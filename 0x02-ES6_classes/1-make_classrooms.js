05bdee7eeae29681a9b7ea88bb14b169d791a651
Benedict Owusu <beneyjoy@gmail.com>
2024-04-25 12:57:14 +0000

    fixing

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from "./0-classroom.js";
 
-function initializeRooms() {
+export default function initializeRooms() {
   const rooms = [];
 
   // Create 3 ClassRoom objects with specified sizes
