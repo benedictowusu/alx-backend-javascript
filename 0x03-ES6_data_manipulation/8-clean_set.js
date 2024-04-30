22ece97d5dd39e9ca063f995097b93d3b958d8b7
Benedict Owusu <beneyjoy@gmail.com>
2024-04-30 14:43:37 +0000

    fixing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -8,16 +8,11 @@
  */
 export default function cleanSet(set, startString) {
   const parts = [];
-  if (
-    !set ||
-    !startString ||
-    !(set instanceof Set) ||
-    typeof startString !== "string"
-  ) {
-    return "";
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
   for (const value of set.values()) {
-    if (typeof value === "string" && value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== value) {
@@ -25,5 +20,5 @@ export default function cleanSet(set, startString) {
       }
     }
   }
-  return parts.join("-");
+  return parts.join('-');
 }
